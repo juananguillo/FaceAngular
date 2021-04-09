8f44622b852ad0e8bad5b3888cbd7cf6f1c1f872
USERSAD\jguillol <jguillol@everis.com>
2021-04-09 12:16:59 +0200

    Creacion del proyecto Spring

---
+++ face-angular/src/main/java/com/everis/faceangular/app/FaceAngularApplication.java
@@ -0,0 +1,13 @@
+package com.everis.faceangular.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceAngularApplication.class, args);
+	}
+
+}
---
+++ face-angular/src/test/java/com/everis/faceangular/app/FaceAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.faceangular.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
